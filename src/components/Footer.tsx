6b469ff520ec247a9a29c4a79f60e7a01dc53fc2
Kodjo AMOUZOU <phipsipy@gmail.com>
2026-01-17 22:35:59 +0000

    V2 déploiement

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,13 @@
 
-import { Container, Facebook, Twitter, Youtube } from "lucide-react"
+import { Container } from "lucide-react"
+import { FaFacebook, FaYoutube, FaWhatsapp } from "react-icons/fa"
+import { FaXTwitter } from "react-icons/fa6"
+
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center  p-10">
-            <aside>
+        <footer className="flex flex-col md:flex-row justify-between items-center gap-4 p-6  ">
+            <aside className="flex flex-col justify-center items-center">
 
                 <Container className="w-10 h-10" />
                 <p className="font-bold">
@@ -15,15 +18,20 @@ const Footer = () => {
             </aside>
             <nav>
                 <div className="grid grid-flow-col gap-4">
-                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                        <Twitter className="w-6 h-6 text-current" />
+                    <a href="https://web.facebook.com/profile.php?id=100069193186621" target="_blank" className="rounded-full border text-accent border-accent p-2 text-3xl hover:bg-accent hover:text-white  hover:-translate-y-1" >
+                        <FaFacebook />
                     </a>
-                    <a href="https://www.youtube.com/@programmationInformatique" target="_blank" rel="noopener noreferrer">
-                        <Youtube className="w-6 h-6 text-current" />
+                    <a href="https://wa.me/2250777444444" target="_blank" className="rounded-full border text-accent border-accent p-2 text-3xl hover:bg-accent hover:text-white  hover:-translate-y-1">
+                        <FaWhatsapp />
                     </a>
-                    <a href="https://web.facebook.com/profile.php?id=100069193186621" target="_blank" rel="noopener noreferrer">
-                        <Facebook className="w-6 h-6 text-current" />
+                    <a href="https://www.youtube.com/@programmationInformatique" target="_blank" className="rounded-full border text-accent border-accent p-2 text-3xl hover:bg-accent hover:text-white  hover:-translate-y-1">
+                        <FaYoutube />
                     </a>
+
+                    <a href="https://twitter.com" target="_blank" className="rounded-full border text-accent border-accent p-2 text-3xl hover:bg-accent hover:text-white  hover:-translate-y-1">
+                        <FaXTwitter/>
+                    </a>
+                    
                 </div>
             </nav>
         </footer>
